0f2680418b6836c9f41067527217ceb1a3e53e24
Mahmoud Darwish <imahmwd14@gmail.com>
2025-11-03 15:45:44 +0700

    chore: remove legacy src/ and archive/ after merge into main.py

--- archive/run_all.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-    [string]$InputDir = "/Users/mahmoud/Desktop/VIDS",
-    [int]$Target = 150
-)
-
-uv run python main.py run-all -i $InputDir -t $Target
-
-
--- archive/run_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-uv run python main.py run-all -i "/Users/mahmoud/Desktop/VIDS" -t 150
-
-
